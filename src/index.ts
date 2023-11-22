cfeb57c708bf1676f0b305e7332ef0c11d8f2727
ShashankRaghuraj <80557791+ShashankRaghuraj@users.noreply.github.com>
2023-11-21 19:01:08 -0500

    cleaning and adding comments

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
+// Importing necessary modules and dependencies
 import { Context, Hono } from 'hono';
-const app = new Hono();
 import * as fs from 'fs';
 import csvParser from 'csv-parser';
-// Import Firebase and its modules
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, get } from 'firebase/database';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
+// Defining the Employee interface to represent employee data
 interface Employee {
     name: string;
     department: string;
@@ -16,98 +16,115 @@ interface Employee {
     skills: string[];
 }
 
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC0bVYMlcFtrx47jeJNyLzeP6URvWtBoAU",
-    authDomain: "organization-data-44cfc.firebaseapp.com",
-    databaseURL: "https://organization-data-44cfc-default-rtdb.firebaseio.com",
-    projectId: "organization-data-44cfc",
-    storageBucket: "organization-data-44cfc.appspot.com",
-    messagingSenderId: "979375300051",
-    appId: "1:979375300051:web:651b7f8491ce6f267a2326",
-    measurementId: "G-LW1MYCZLHF"
-  };
+  apiKey: "AIzaSyC0bVYMlcFtrx47jeJNyLzeP6URvWtBoAU",
+  authDomain: "organization-data-44cfc.firebaseapp.com",
+  databaseURL: "https://organization-data-44cfc-default-rtdb.firebaseio.com",
+  projectId: "organization-data-44cfc",
+  storageBucket: "organization-data-44cfc.appspot.com",
+  messagingSenderId: "979375300051",
+  appId: "1:979375300051:web:651b7f8491ce6f267a2326",
+  measurementId: "G-LW1MYCZLHF"
+};
 
+// Defining the Department interface to represent department data
 interface Department {
     name: string;
     managerName: string;
     employees: Employee[];
 }
 
+// Defining the Organization interface to represent the overall structure
 interface Organization {
     departments: Department[];
 }
 
+// Creating an instance of the Hono application
+const app = new Hono();
+
+// Handling the root endpoint
 app.get("/", (ctx) => {
     return ctx.text(
-        //creating a table of contents
-        "This is the cloudfare general coding assesment challenge \n\n1) type in /organization-chart to see the organization chart\n2)type in /me to get information in a json file about me!"
-    )
+        // Providing information on available endpoints
+        "This is the Cloudflare general coding assessment challenge \n\n1) GET/organization-chart to see the organization chart\n2) GET/me to get information in a JSON file about me!"
+    );
 });
 
+// Handling the organization chart endpoint
 app.get("/organization-chart", async (ctx) => {
-    // Initialize Firebase
+    // Initializing Firebase app and retrieving data
     const firebaseApp = initializeApp(firebaseConfig);
     const database = getDatabase(firebaseApp);
     const db = getFirestore(firebaseApp);
-    //console.log(db);
+
     try {
+        // Retrieving organization data from Firestore
         const organization = collection(db, 'departments');
         const organizationSnapshot = await getDocs(organization);
-        
+
+        // Mapping Firestore data to a structured organization format
         const departments = organizationSnapshot.docs.map(departmentDoc => {
-          const departmentData = departmentDoc.data();
-        
-          const manager = departmentData.employees.find(employee => employee.isManager);
-          const managerName = manager ? manager.name : "";
-        
-          const employees = departmentData.employees.map(employee => {
+            const departmentData = departmentDoc.data();
+
+            // Extracting manager information and employee details
+            const manager = departmentData.employees.find(employee => employee.isManager);
+            const managerName = manager ? manager.name : "";
+            const employees = departmentData.employees.map(employee => {
+                return {
+                    name: employee.name,
+                    department: departmentData.name,
+                    salary: employee.salary,
+                    office: employee.office,
+                    isManager: employee.isManager,
+                    skills: employee.skills
+                };
+            });
+
+            // Constructing department object
             return {
-              name: employee.name,
-              department: departmentData.name,
-              salary: employee.salary,
-              office: employee.office,
-              isManager: employee.isManager,
-              skills: employee.skills
+                name: departmentData.name,
+                managerName: managerName,
+                employees: employees
             };
-          });
-        
-          return {
-            name: departmentData.name,
-            managerName: managerName,
-            employees: employees
-          };
         });
-        
+
+        // Constructing the final result object
         const result = {
-          departments: departments
+            departments: departments
         };
-        
+
+        // Returning the result as a formatted JSON
         return ctx.text(JSON.stringify(result, null, 2));
     } catch (error) {
+        // Handling errors during data retrieval
         console.error('ERROR fetching organization chart data.' + error);
         return ctx.text('ERROR fetching organization chart data.' + error);
     }
 });
 
+// Handling the "me" endpoint
 app.get("/me", async (ctx) => {
+    // Initializing Firebase app and retrieving data
     const firebaseApp = initializeApp(firebaseConfig);
     const database = getDatabase(firebaseApp);
     const db = getFirestore(firebaseApp);
     const organization = collection(db, 'aboutMe');
     const organizationSnapshot = await getDocs(organization);
+
+    // Mapping Firestore data to a structured "me" format
     const jsonValues = organizationSnapshot.docs.map(doc => {
         const data = doc.data();
 
-        // Assuming the fields exist in the Firebase documents
-        const name = data.name || "";  // Replace with the actual field name in your Firebase document
-        const homepage = data.homepage || "";  // Replace with the actual field name in your Firebase document
-        const githubURL = data.githubURL || "";  // Replace with the actual field name in your Firebase document
-        const interestingFact = data.interestingFact || "";  // Replace with the actual field name in your Firebase document
-        const skills = data.skills || [];  // Replace with the actual field name in your Firebase document
+        // Extracting relevant fields from Firebase document
+        const name = data.name || "";
+        const homepage = data.homepage || "";
+        const githubURL = data.githubURL || "";
+        const interestingFact = data.interestingFact || "";
+        const skills = data.skills || [];
 
+        // Constructing the "me" object
         return {
             name,
             homepage,
@@ -116,7 +133,12 @@ app.get("/me", async (ctx) => {
             skills,
         };
     });
-    return ctx.text(JSON.stringify(jsonValues, null, 2));
 
+    // Returning the "me" data as a formatted JSON
+    return ctx.text(JSON.stringify(jsonValues, null, 2));
 });
+
+// Exporting the Hono application instance
 export default app;
+
+
